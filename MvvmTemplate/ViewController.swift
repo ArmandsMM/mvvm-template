6d34e8e478de7b72288ff22620a1527bdfbd4876
Armands Mikanovskis <mikanovskis@gmail.com>
2020-04-16 14:16:48 +0300

    Initial Commit

---
+++ MvvmTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvvmTemplate
+//
+//  Created by armands.mikanovskis on 16/04/2020.
+//  Copyright © 2020 armands.mikanovskis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
